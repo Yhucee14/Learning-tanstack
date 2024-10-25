757120bb98353bd59e22b7e994883f9a007c8031
Yhucee14 <uchechukwu7814@gmail.com>
2024-10-25 12:20:18 -0700

    tanstack

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/todos")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return <>{JSON.stringify(data)}</>;
+}
+
+export default App;
