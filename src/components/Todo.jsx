ef61dcccb04a5c0b63a2b88d88043ebea62a72e1
Yhucee14 <uchechukwu7814@gmail.com>
2024-10-27 23:46:26 -0700

    today

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,23 +4,12 @@ import { useIsFetching } from '@tanstack/react-query';
 
 const Todo = () => {
 const todosIdsQuery = useTodosIds();
-const isFetching = useIsFetching();
 
-// if (todosIdsQuery.isPending){
-//     return <span>Loading...</span>
-// }
-
-// if (todosIdsQuery.isError){
-//     return <span>Error ocurred...</span>
-// }
 
   return (
     <>
-    <p>Query function status: {todosIdsQuery.fetchStatus}</p>
-    <p>Query data status: {todosIdsQuery.status}</p>
-    <p>Global isFetching: {isFetching}</p>
     {todosIdsQuery.data?.map((id) => {
-      return <p key={id}>{id}</p>
+      return <p key={id}>id: {id}</p>
     })}
     </>
   )
